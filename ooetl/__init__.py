ef60c076d8930852880bca8809306b7cf204cd9a
guglielmo <guglielmo@openpolis.it>
2021-09-02 23:06:09 +0200

    Bump version: 1.1.3 → 1.1.4

--- ooetl/__init__.py
+++ ooetl/__init__.py
@@ -3,7 +3,7 @@ import logging
 
 from .transformations import DummyTransformation
 
-__version__ = '1.1.3'
+__version__ = '1.1.4'
 __description__ = "A framework to write ETL processes at the Openpolis Foundation."
 
 """A framework to write ETL processes at the Openpolis Foundation.
